13e9400e2bca004e63bf6f282d32466fa6f78feb
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-12 21:37:43 +0000

    automatically backed up by learn

--- db/migrate/002_create_schoolclasses.rb
+++ db/migrate/002_create_schoolclasses.rb
@@ -1,6 +1,8 @@
 class CreateSchoolclasses < ActiveRecord::Migration
   def change
     create_table :schoolclasses do |t|
+      t.string :title
+      t.integer :room_number
 
       t.timestamps null: false
     end
