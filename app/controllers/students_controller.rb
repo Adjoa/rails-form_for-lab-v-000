6028c7edefc986b98bbe6789e28276f7c130e8ee
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-12 22:10:46 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,6 +5,7 @@ class StudentsController < ApplicationController
     end
 
     def create
+      raise params.inspect
       @student = Student.new(student_params(:first_name, :last_name))
       @student.save
       redirect_to student_path(@student)
